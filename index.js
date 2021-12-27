827c2fddbdae38655d3031fd7afdddf45424ac26
redoverflow <59606818+redoverflow@users.noreply.github.com>
2021-12-27 12:04:40 +0300

    edit milestones

--- index.js
+++ index.js
@@ -8,8 +8,10 @@ console.log(endofeyho);
 var untilendofeyho = countdown(null, endofeyho);
 var countdownel = document.getElementById('countdown');
 var untiltxt = document.getElementById('untiltxt');
+var milestone1 = document.getElementById('milestone1');
 untiltxt.innerHTML = 'left until eat your heart out <b>'+(parseInt(currentyear)+1).toString()+"</b> redoverflow"
 countdownel.innerHTML = untilendofeyho.toString();
+milestone1.innerHTML = milestone1.innerHTML + " (<b><em>" + countdown(null, new Date(2021, 10, 1), countdown.DAYS).toString() + " ago</em></b>)";
 
 var countdownintfunc = setInterval(() => {
     var untilendofeyho = countdown(null, endofeyho);
